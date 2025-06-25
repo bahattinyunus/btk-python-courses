eac9ecc33e48355d25639c36f8d7adb778a29e66
bahattinyunuscetin <bahattinyunuscetin@hotmail.com>
2025-06-25 13:30:19 +0300

    update

--- course_2/bolum-7/uygulama-karsilastirma-operatorleri.py
+++
@@ -1,7 +0,0 @@
-not1 = int(input("not 1: "))
-not2 = int(input("not 2: "))
-not3 = int(input("not 3: "))
-
-ortalama = (not1 + not2 + not3) / 3
-
-print(f"öğrencinin not ortalaması: {round(ortalama,2)}, başarı durumu {ortalama >= 50}")
--- course_2/bolum-7/uygulama-mantiksal-operatorler.py
+++
@@ -1,8 +0,0 @@
-
-
-# İşe girmek için en az önlisans ya da lisans mezunu olma durumunu kontrol ediniz. Sigara kullanmama koşulu.
-egitim = "önlisans"
-sigara_icme = False
-
-sonuc = (egitim == "önlisans" or egitim == "lisans") and (not(sigara_icme))
-
