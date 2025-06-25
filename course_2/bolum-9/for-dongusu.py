eee42220191d23f871c9bd22a77bdac64b8a67b2
bahattinyunuscetin <bahattinyunuscetin@hotmail.com>
2025-06-25 21:33:35 +0300

    update

--- course_2/bolum-9/for-dongusu.py
+++ course_2/bolum-9/for-dongusu.py
@@ -1,21 +1,4 @@
-# for => lis    t
-# while => koşullu
 
-sayilar = [1,2,3,4,6,8,91,21]
-isimler = ["Ali","Canan","Zeynep"]
-adsoyad = "Sadık Turan"
-
-# for x in sayilar:
-#     print(x)
-    
-# for x in sayilar:
-#     print("Merhaba BTK Akademi")
-
-# for isim in isimler:
-#     print(isim)   
-
-# for i in adsoyad:
-#     print(i)
 
 my_tuple = [(1,2),(3,4),(5,6)]
 
@@ -27,10 +10,10 @@ my_dict = {"41":"Kocaeli","53":"Rize","35":"İzmir"}
 # for x in my_dict:
 #     print(my_dict[x])
 
-for x in my_dict.values():
+for x in my_dict.values():    
     print(x)
 
-for x in my_dict.keys():
+for x in my_dict.keys():   
     print(x)
 
 for x,y in my_dict.items():
