c03998cebe593e92c167218b843f66d27665a22a
bahattinyunuscetin <bahattinyunuscetin@hotmail.com>
2025-06-30 09:37:26 +0300

    update

--- course_2/bolum-11/dosya-guncelleme.py
+++ course_2/bolum-11/dosya-guncelleme.py
@@ -2,13 +2,13 @@
 #     file.write("6-Bmw")
 
 # with open("markalar.txt","r+", encoding="utf-8") as file:
-#     markalar = file.read()
+#     markalar = file.read()       #başına eklemek için listeyi diziye al ve tekrar birleştir
 #     markalar = "1-Toyota\n" + markalar
 #     file.seek(0)
 #     file.write(markalar)
 
 with open("markalar.txt","r+",encoding="utf-8") as file:
-    markalar = file.readlines()
+    markalar = file.readlines()    # listeye al ve insert metoduyla ekle
     markalar.insert(2, "3-Renault\n")
     file.seek(0)
     # for marka in markalar:
