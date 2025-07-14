f2916383d88bda8d57105326969aee7bcff90aa4
Bahattin Yunus Çetin <83650878+bahattinyunuscetin@users.noreply.github.com>
2025-07-14 20:08:55 +0300

    update

--- course_2/bolum-14/uygulama-module/app.py
+++ course_2/bolum-14/uygulama-module/app.py
@@ -1,13 +1,3 @@
-"""
-    module1 (db)      : urunler
-    module2 (methods) : urunEkle(), urunGuncelle(), urunleriGetir()
-    module3 (app)     :
-
-        yeni ürün ekleme => urunEkle("iphone 15", 60000)
-        ürün güncelle    => urunGuncelle(1, "iphone 15 pro", 80000)
-        ürünleri listele => urunleriGetir() 
-"""
-
 from methods import *
 
 urunEkle("iphone 20", 90000)
